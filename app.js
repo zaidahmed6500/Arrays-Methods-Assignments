8efe1877d392722b1af28a37aa3bc2ea11e9378f
zaidahmed6500 <zaidahmed6500@gmail.com>
2025-09-27 03:14:16 +0500

    update changes

--- app.js
+++ app.js
@@ -113,18 +113,72 @@ const activeUser = active.filter(item => item.active)
 console.log(activeUser)
 
 
+
+
 // 15 under construction
-const words = [
-  "apple",    // contains 'a'
-  "banana",   // contains 'a'
-  "grape",    // contains 'a'
-  "cat",      // contains 'a'
-  "carrot",   // contains 'a'
-  "blue",     // no 'a'
-  "tree",     // no 'a'
-  "sky",      // no 'a'
-  "moss",     // no 'a'
-  "light"    // no 'a'
-];
+const words = ["apple", "banana", "avocado", "cherry", "apricot"];
+const aWords = words.filter(item => item.startsWith("a"))
+console.log(aWords)
 
 
+
+// 16
+const greaterNum = [1, 3, 18, 6, 8, 10, 13, 5, 25, 41]
+const keepNum = greaterNum.filter(item => item > 10)
+console.log(keepNum)
+
+
+
+// 17
+const mixedArray = [0, 1, false, 2, "", 3, null, "hello", undefined, NaN];
+const falsyvalue = mixedArray.filter(item => Boolean(item))
+// const falsyvalue = mixedArray.filter(item => Boolean(item))
+console.log(falsyvalue)
+
+
+
+// 18 
+const texts = ["coding is fun", "decode", "learn to code", "programming", "codec"];
+const getString = texts.filter(item => item.includes("code"))
+console.log(getString)
+
+
+// 19
+const data = [1, null, 2, undefined, 3, null, 4];
+const cleanData = data.filter(item => item !== null && item !== undefined)
+console.log(cleanData)
+
+
+
+
+// 20
+const products = [{name: "Phone", price: 99}, {name: "Tablet", price: 120}]
+const getProducts = products.filter(item => item.price < 100)
+console.log(getProducts)
+
+
+
+
+// 21
+const numbers = [1, 2, 3, 4];
+const total = numbers.reduce((accumulator,current) => accumulator + current, 0)
+console.log(total)
+
+
+// 22
+const newNumbers = [5, 12, 8, 21, 3];
+
+const maxNumber = newNumbers.reduce((max, current) => Math.max(max, current));
+
+console.log(maxNumber);
+
+
+
+// 23
+const newFruits = ["apple", "banana", "apple", "orange", "banana"];
+
+const counts = newFruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+console.log(counts);
